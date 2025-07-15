d6a5205baff73b962e4577276c9bc0f4a73ec2aa
Anilch12 <anilch5199@gmail.com>
2025-07-15 10:15:59 +0530

    hot fix

--- src/component/Portfolio.jsx
+++ src/component/Portfolio.jsx
@@ -80,8 +80,7 @@ const Portfolio = () => {
             <h4>Software Developer</h4>
             <p>Based in Hyderabad, Telangana</p>
             <p>
-              2.5+ years of experience in front-end development and data
-              scraping.
+              2.5+ years of experience in front-end development
             </p>
 
             {/* <!-- Button trigger modal --> */}
@@ -135,12 +134,12 @@ const Portfolio = () => {
       </div>
 
       {/* About Section */}
-      <div className="text-center" id="about">
+      <div id="about">
         <section  className="container py-5 fade-bright">
-          <h2 className="text-warning">About Me</h2>
+          <h2 className="text-warning text-center">About Me</h2>
           <p>
             Passionate and detail-focused Front-End Developer with 2.5+ years of experience in creating responsive and
-            user- friendly web applications. Skilled in React.js, Next.js, JavaScript, SCSS, and Bootstrap. Good at working
+            user- friendly web applications. Skilled in React.js, Next.js, JavaScript and Bootstrap. Good at working
             with APIs and turning UI/UX designs into clean, working websites. Strong in solving problems and working
             with backend teams to build complete web solutions.
           </p>
@@ -181,27 +180,33 @@ const Portfolio = () => {
           <ul className="list-group border-0 black list-unstyled">
             <li className="black border-0 mb-2">
               <strong className="mt-2">1. AIMS360</strong> 
-              <p className="ms-3 mt-2">Duration: 2 Years | Role: Front-End Developer</p>
-              <p className="ms-3">Technologies: React.js, CSS, Bootstrap, REST APIs</p>
-              <p className="ms-3">Built clean and scalable UI modules for HRM, customer records, lead tracking, invoicing, and user profile management.</p>
-              <p className="ms-3">Handled API integration and UI optimization for smooth business workflow management.</p>
+              <p className="ms-3 mt-2"><strong>Duration:</strong> 2 Years | Role: Front-End Developer</p>
+              <div className="ms-3">
+                <p className="ms-3">Technologies: React.js, CSS, Bootstrap, REST APIs</p>
+                <p className="ms-3">Built clean and scalable UI modules for HRM, customer records, lead tracking, invoicing, and user profile management.</p>
+                <p className="ms-3">Handled API integration and UI optimization for smooth business workflow management.</p>
+              </div>
              </li>
 
             <li className="black border-0 mb-2">
               <strong>2. E-commerce Application:</strong> 
-              <p className="ms-3 mt-2">Duration: 5 Months | Role: Front-End Developer</p>
-              <p className="ms-3">Technologies: React.js, HTML, CSS, Bootstrap, React Strap, JavaScript, API Integration Designed and developed a responsive webpage.</p>
-              <p className="ms-3">Built reusable UI components for risk tracking, mitigation dashboards, and control monitoring.</p>
-              <p className="ms-3">Implemented domain-based UI restrictions and responsive layouts across all devices.</p>
+              <p className="ms-3 mt-2"><strong>Duration:</strong> 5 Months | Role: Front-End Developer</p>
+              <div className="ms-3">
+                <p className="ms-3">Technologies: React.js, HTML, CSS, Bootstrap, React Strap, JavaScript, API Integration Designed and developed a responsive webpage.</p>
+                <p className="ms-3">Built reusable UI components for risk tracking, mitigation dashboards, and control monitoring.</p>
+                <p className="ms-3">Implemented domain-based UI restrictions and responsive layouts across all devices.</p>
+              </div>
 
             </li>
             <li className="black border-0 mb-2">
               <strong>3. Data Scraping:</strong> 
-              <p className="ms-3 mt-2">Duration: 5 Months | Role: Support Role</p>
-              <p className="ms-3">Technologies: Python, SQL Database, Beautiful Soup, requests, </p>
-              <p className="ms-3">Extracted structured and unstructured data from various websites for analysis and reporting.</p>
-              <p className="ms-3">Handled dynamic content, pagination, and login-based sites with advanced scraping techniques</p>
-              <p className="ms-3">Developed and implemented custom web scraping scripts.</p>
+              <p className="ms-3 mt-2"><strong>Duration:</strong> 5 Months | Role: Support Role</p>
+              <div className="ms-3">
+                <p className="ms-3">Technologies: Python, SQL Database, Beautiful Soup, requests, </p>
+                <p className="ms-3">Extracted structured and unstructured data from various websites for analysis and reporting.</p>
+                <p className="ms-3">Handled dynamic content, pagination, and login-based sites with advanced scraping techniques</p>
+                <p className="ms-3">Developed and implemented custom web scraping scripts.</p>
+              </div>
             </li>
           </ul>
         </section>
